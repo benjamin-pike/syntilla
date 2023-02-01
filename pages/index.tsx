ac0a7003bb85785274dbd59477dd6a0b9bc73fa6
Benjamin Pike <ben.pike@pepperhq.com>
2023-02-01 23:39:25 +0000

    add stats button, add streak counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,9 @@ import Content from '../components/Content'
 import Modal from '../components/Modal'
 import { useState } from 'react'
 import { HiOutlineCog6Tooth } from 'react-icons/hi2'
+import { IoMdHeartEmpty } from 'react-icons/io'
+import { AiOutlineFire } from 'react-icons/ai'
 import { SettingsContextProvider } from '../store/settings.context'
-import BMCLogoBase from '../public/bmc-logo-no-bg.svg'
-import BMCLogoHover from '../public/bmc-logo-yellow-bg.svg'
-import DonateIcon from '../public/donate.svg'
 
 export default function Index() {
     const [modalStatus, setModalStatus] = useState<'open' | 'closing' | 'closed'>('closed')
@@ -19,25 +18,37 @@ export default function Index() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
             <header id = {styles.header}>
-                <h1 data-text = 'Syntilla'>Syntilla</h1>
-                <button
-                    id = {styles.donateButton}
-                    className = {styles.headerButton}
-                    onClick = {() => window.open('https://www.buymeacoffee.com/benjpike', '_blank')}
-                >
-                    {/* <span>
-                        <BMCLogoBase id = { styles.bmcBase } />
-                        <BMCLogoHover id = { styles.bmcHover } />
-                    </span> */}
-                    <DonateIcon />
-                </button>
-                <button
-                    id = {styles.settingsButton}
-                    className = {styles.headerButton}
-                    onClick = {() => setModalStatus('open')}
-                >
-                    <HiOutlineCog6Tooth color='white' width = '2em'/>
-                </button>
+                <div id = {styles.headerContent}>
+                    <button
+                        id = {styles.statsButton}
+                        className = {styles.headerButton}
+                        onClick = {() => {}}
+                    >
+                        <div id = {styles.statsBars}>
+                            <div className = {styles.statsBar} />
+                            <div className = {styles.statsBar} />
+                            <div className = {styles.statsBar} />
+                        </div>
+                    </button>
+                    <div id = {styles.dailyStreak}>
+                        <AiOutlineFire />
+                    </div>
+                    <h1 data-text = 'Syntilla'>Syntilla</h1>
+                    <button
+                        id = {styles.donateButton}
+                        className = {styles.headerButton}
+                        onClick = {() => window.open('https://www.buymeacoffee.com/benjpike', '_blank')}
+                    >
+                        <IoMdHeartEmpty />
+                    </button>
+                    <button
+                        id = {styles.settingsButton}
+                        className = {styles.headerButton}
+                        onClick = {() => setModalStatus('open')}
+                    >
+                        <HiOutlineCog6Tooth />
+                    </button>
+                </div>
             </header>
 			<main className = { styles.main }>
                 <SettingsContextProvider>
